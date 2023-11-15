0429c650b68462cc68308be5fad869b881adb29d
mhore22 <padillawilmor@gmail.com>
2023-11-15 10:30:04 +0800

    Script to test Outbound Email

---
+++ TestOutboundEmail.ps1
@@ -0,0 +1,13 @@
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+ 
+#Configuration Parameters
+$SiteURL="########"
+$Email = "########"
+$Subject = "Test Email from SharePoint"
+$Body = "Test Email Body"
+ 
+#Get the Web 
+$Web = Get-SPWeb $SiteURL
+ 
+#Send Email using SPUtility SendEmail method
+[Microsoft.SharePoint.Utilities.SPUtility]::SendEmail($Web ,0,0,$Email,$Subject,$Body)
